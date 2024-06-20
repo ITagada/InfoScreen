64a62b2c916b5e164246cbfa7ee01ff9cf042b88
Polovnikov Maksim <m_polovnikov@inbox.ru>
2024-06-20 08:51:11 +0300

    featch: Дополнена функция обрабатывающая стартовый экран (добавлен функционал перенаправления конечного устройства в зависимости от поступивших данных от него)

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path("get_screen_info/", views.get_screen_info, name='get_screen_info'),
+    path('get_screen_info/', views.get_screen_info, name='get_screen_info'),
+    path('get_screen_info2/', views.get_screen_info2, name='get_screen_info2'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,23 +1,27 @@
 
 import json
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+
 
 def index(request):
-    return render(request, 'main/index.html')
-
-# @csrf_exempt
-def get_screen_info(request):
     if request.method == "POST":
         try:
             data = json.loads(request.body)
             screen_width = data.get('screen_width')
             screen_height = data.get('screen_height')
-
             return JsonResponse({'screen_width': screen_width, 'screen_height': screen_height})
         except json.JSONDecodeError:
             return JsonResponse({'status': 'fail1', 'message': 'Invalid JSON1'}, status=400)
 
+    if JsonResponse({'screen_width': 1920}) and JsonResponse({'screen_height': 1080}):
+        return redirect('get_screen_info')
+    else:
+        return redirect('get_screen_info2')
+
+def get_screen_info(request):
     return render(request, 'main/get_screen_info.html')
+
+def get_screen_info2(request):
+    return render(request, 'main/get_screen_info2.html')
