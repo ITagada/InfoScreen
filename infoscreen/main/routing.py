67cd40e9a097fe21b6715aa5fd5b91784d0780b5
Polovnikov Maksim <m_polovnikov@inbox.ru>
2024-07-15 13:15:33 +0300

    Delete

--- infoscreen/main/routing.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path
-
-from . import consumers
-
-
-""" Тут мы описываем маршрут открытия сокета"""
-websocket_urlpatterns = [
-    re_path(r'ws/socket-server/$', consumers.ScreenConsumer.as_asgi()),
-]
--- main/static/main/js/rediction_delay.js
+++
@@ -1,9 +0,0 @@
-let number = 0
-let remainigSeconds = 10;
-        const intervalID = setInterval(() => {
-            document.getElementById('countdown-timer').textContent = `Переход на новую страницу через ${remainigSeconds--} сек`;
-            if (remainigSeconds < 0) {
-                clearInterval(intervalID);
-                window.location = 'http://127.0.0.1:8000/get_screen_info/'
-            }
-        }, 1000)
